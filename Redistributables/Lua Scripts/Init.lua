c6ef34ad25a19906960fb59b5c95bb546e0681eb
depthso <86912923+depthso@users.noreply.github.com>
2023-10-06 16:25:11 +0100

    Update Init.lua

--- Redistributables/Lua Scripts/Init.lua
+++ Redistributables/Lua Scripts/Init.lua
@@ -1,19 +1,18 @@
-  --[[
- *
- * ░▒█░░░░▀█▀░█▀▀▄░░░▀░█▀▀░█▀▄░▀█▀░▄▀▀▄░█▀▀▄░░░▒█▀▀▀░█░▒█░█▀▀▄░█▀▄░▀█▀░░▀░░▄▀▀▄░█▀▀▄░█▀▀░░
- * ░▒█░░░░▒█░░█░▒█░░░█░█▀▀░█░░░░█░░█░░█░█▄▄▀░░░▒█▀▀░░█░▒█░█░▒█░█░░░░█░░░█▀░█░░█░█░▒█░▀▀▄░░
- * ░▒█▄▄█░▄█▄░▀░░▀░█▄█░▀▀▀░▀▀▀░░▀░░░▀▀░░▀░▀▀░░░▒█░░░░░▀▀▀░▀░░▀░▀▀▀░░▀░░▀▀▀░░▀▀░░▀░░▀░▀▀▀░░
- *
- * Written By Depso
- *
- * ]]
-
+ --[[
+*
+* ░▒█░░░░▀█▀░█▀▀▄░░░▀░█▀▀░█▀▄░▀█▀░▄▀▀▄░█▀▀▄░░░▒█▀▀▀░█░▒█░█▀▀▄░█▀▄░▀█▀░░▀░░▄▀▀▄░█▀▀▄░█▀▀░░
+* ░▒█░░░░▒█░░█░▒█░░░█░█▀▀░█░░░░█░░█░░█░█▄▄▀░░░▒█▀▀░░█░▒█░█░▒█░█░░░░█░░░█▀░█░░█░█░▒█░▀▀▄░░
+* ░▒█▄▄█░▄█▄░▀░░▀░█▄█░▀▀▀░▀▀▀░░▀░░░▀▀░░▀░▀▀░░░▒█░░░░░▀▀▀░▀░░▀░▀▀▀░░▀░░▀▀▀░░▀▀░░▀░░▀░▀▀▀░░
+*
+* Written By Depso
+*
+* ]]
 local EXPLOIT_NAME = "LInjector"
-local EXLPOIT_VERSION = "18.09.2023"
+local EXLPOIT_VERSION = "6.10.2023"
 
 local genv = getgenv()
 if genv[EXPLOIT_NAME] then
-	return script:Remove()
+   return script:Remove()
 end
 genv[EXPLOIT_NAME] = true
 
@@ -25,67 +24,64 @@ local localplayer=game:GetService'Players'.LocalPlayer
 -------------
 
 local hashlibalgs = {
-	"sha1", "sha224"
+   "sha1", "sha224"
 }
 local hashalgs = {
-	"md5", "sha1", "sha224", "sha256", "sha384", "sha512", "sha3-256", "sha3-384", "sha3-512",
-	"md2", "haval", "ripemd128", "ripemd160", "ripemd256", "ripemd320"
+   "md5", "sha1", "sha224", "sha256", "sha384", "sha512", "sha3-256", "sha3-384", "sha3-512",
+   "md2", "haval", "ripemd128", "ripemd160", "ripemd256", "ripemd320"
 }
 local ciphers = {
-	['aes-cbc'] = "CBC",
-	['aes-cfb'] = "CFB",
-	['aes-ctr'] = "CTR",
-	['aes-ofb'] = "OFB",
-	['aes-gcm'] = "GCM"
+   ['aes-cbc'] = "CBC",
+   ['aes-cfb'] = "CFB",
+   ['aes-ctr'] = "CTR",
+   ['aes-ofb'] = "OFB",
+   ['aes-gcm'] = "GCM"
 }
 
 
-function Export(name, value)
-	getgenv()[name] = value
+Export=function(name, value)
+   getgenv()[name] = value
 end
+STDExport=function(text)
+   writefile("LINJECTOR/LINJECTOR.li", text)
+   wait()
+end 
 
-Export("identifyexecutor", function()
-	return EXPLOIT_NAME, EXLPOIT_VERSION
-end)
-Export("getexecutorname", function()
-	return EXPLOIT_NAME, EXLPOIT_VERSION
-end)
-Export("disassemble", disassemble)
-
+---------
 
 local Oldcrypt = crypt
 local NewCrypt = Oldcrypt
 
 NewCrypt.encrypt = function(cipher, data, key, nonce)
-	cipher = cipher:lower()
-	if cipher:find("eax") or cipher:find("bf") then
-		return ""
-	end
-	return crypt.custom_encrypt(data, key, nonce, ciphers[cipher:gsub("_", "-")])
+   cipher = cipher:lower()
+   if cipher:find("eax") or cipher:find("bf") then
+      return ""
+   end
+   return crypt.custom_encrypt(data, key, nonce, ciphers[cipher:gsub("_", "-")])
 end
 NewCrypt.decrypt = function(cipher, data, key, nonce)
-	cipher = cipher:lower()
-	if cipher:find("eax") or cipher:find("bf") then
-		return ""
-	end
-	return crypt.custom_decrypt(data, key, nonce, ciphers[cipher:gsub("_", "-")])
+   cipher = cipher:lower()
+   if cipher:find("eax") or cipher:find("bf") then
+      return ""
+   end
+   return crypt.custom_decrypt(data, key, nonce, ciphers[cipher:gsub("_", "-")])
 end
 NewCrypt.hash = function(alg, data)
-	alg = alg:lower():gsub("_", "-")
+   alg = alg:lower():gsub("_", "-")
 
-	local HashLib = table.find(hashlibalgs, alg)
-	local SwLib = table.find(hashalgs, alg)
-	assert(HashLib or SwLib, "#1 Unknown hash algorithm")
+   local HashLib = table.find(hashlibalgs, alg)
+   local SwLib = table.find(hashalgs, alg)
+   assert(HashLib or SwLib, "#1 Unknown hash algorithm")
 
-	if HashLib then
-		return hash[alg:gsub("-", "_")](data)
-	end
-	if SwLib then
-		return Oldcrypt.hash(data, alg):lower()
-	end
+   if HashLib then
+      return hash[alg:gsub("-", "_")](data)
+   end
+   if SwLib then
+      return Oldcrypt.hash(data, alg):lower()
+   end
 end
 NewCrypt.derive = function(_, len)
-	return Oldcrypt.generatebytes(len)
+   return Oldcrypt.generatebytes(len)
 end
 NewCrypt.random = Oldcrypt.generatebytes 
 NewCrypt.generatebytes = Oldcrypt.generatebytes
@@ -107,46 +103,55 @@ Export("custom", NewCrypt)
 Export("crypt", NewCrypt)
 Export("crypto", NewCrypt)
 
+Export("identifyexecutor", function()
+   return EXPLOIT_NAME, EXLPOIT_VERSION
+end)
+Export("getexecutorname", function()
+   return EXPLOIT_NAME, EXLPOIT_VERSION
+end)
+Export("disassemble", disassemble)
+Export("decompile", disassemble)
+ 
+
 setreadonly(crypt, true)
 setreadonly(crypto, true)
 setreadonly(custom, true)
 
 local Functions={
-	["messagebox"]="showmsg",
-	["setDiscordRPC"]="setrpc",
-	["rconsoleprint"]="rprintconsole",
-	["rconsoleinfo"]="rconsoleinfo",
-	["rconsolename"]="rconsolename",
-	["rconsolewarn"]="rconsolewarn",
-	["rconsoleerr"]="rconsoleerr",
-	["toclipboard"]="toClipboard",
-	["rconsoleclose"]="closeconsole",
-	["rconsoleshow"]="showconsole",
-	["rconsoleclear"]="consoleclear",
+  "messagebox",
+  "setDiscordRPC",
+  "rconsoleprint",
+  "rconsoleinfo",
+  "rconsolename",
+  "rconsolewarn",
+  "rconsoleerr",
+  "toclipboard",
+  "rconsoleclose",
+  "rconsoleshow",
+  "rconsoleclear"
 }
 
-STDExport=function(text)
-	writefile("LINJECTOR/LINJECTOR.li", text)
+for _, name in next, Functions do
+   Export(name,function(...)
+      local String, args="",table.pack(...)
+      for i=1, args.n do
+         String=("%s|||%s"):format(String, tostring(args[i]))
+      end
+      STDExport(('%s%s'):format(name, String))
+   end)
 end
+wait()
 
-for name, func in pairs(Functions) do
-	Export(name,function(...)
-		local String, args="",table.pack(...)
-		for i=1, args.n do
-			String=("%s|||%s"):format(String, tostring(args[i]))
-		end
-		STDExport(('%s%s'):format(name, String))
-	end)
-end
-
-Export("rprintconsole",rconsoleprint)
 Export("setclipboard",toclipboard)
 Export("set_clipboard",toclipboard)
 Export("set_clipboard",toclipboard)
 Export("Clipboard",{set=toclipboard})
 
 pcall(function()
-	SendFunction(('welcome|||%s|||%s'):format(localplayer.DisplayName, MarketplaceService:GetProductInfo(game.PlaceId).Name))
+   SendFunction(('welcome|||%s|||%s'):format(
+      localplayer.DisplayName, 
+      MarketplaceService:GetProductInfo(game.PlaceId).Name
+   ))
 end)
  
  --[[
@@ -160,8 +165,8 @@ end)
  * 
 
  if linjector then
-	script:Remove()
-	return 
+   script:Remove()
+   return 
 end
 getgenv()["linjector"] = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ItzzExcel/LInjector/master/Redistributables/Lua%20Scripts/Init.lua"))()]]
