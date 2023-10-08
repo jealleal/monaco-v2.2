51a8a208fb6029541bc8b59f79b32f767fe856ea
ItzzExcel <98148217+ItzzExcel@users.noreply.github.com>
2023-10-08 01:24:46 -0600

    Update again

--- LInjector/LInjector/Properties/AssemblyInfo.cs
+++ LInjector/LInjector/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("23.09.2023")]
+[assembly: AssemblyFileVersion("08.10.2023")]
